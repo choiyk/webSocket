8d824949f2c6106407f0c5ed77b04d17234f3e6e
choiyk <d_pvmf@naver.com>
2018-06-12 21:01:55 +0900

    commit

---
+++ webSocketEx/src/main/java/com/kyung/WebSocketExApplication.java
@@ -0,0 +1,12 @@
+package com.kyung;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebSocketExApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebSocketExApplication.class, args);
+	}
+}
